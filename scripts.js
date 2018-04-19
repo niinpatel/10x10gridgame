fbbb335678a25e09d7741cd40475eebf964b824e
Nitin <nitinpatel278@gmail.com>
2018-04-19 12:05:13 +0530

    design and optimization

--- scripts.js
+++ scripts.js
@@ -1,28 +1,28 @@
 // get the reference for the body
-var body = document.getElementsByTagName("body")[0];
+let body = document.getElementsByTagName("body")[0];
 
 // creates a <table> element and a <tbody> element
-var tbl = document.createElement("table");
-var tblBody = document.createElement("tbody");
+let tbl = document.createElement("table");
+let tblBody = document.createElement("tbody");
 
 index = 1;
 // creating all cells
-for (var i = 1; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     // creates a table row
-    var row = document.createElement("tr");
+    let row = document.createElement("tr");
 
-    for (var j = 1; j <= 10; j++) {
+    for (let j = 1; j <= 10; j++) {
         // Create a <td> element and a text node, make the text
         // node the contents of the <td>, and put the <td> at
         // the end of the table row
-        var cell = document.createElement("td");
-        var cellText = document.createTextNode(index);
+        let cell = document.createElement("td");
+        let cellText = document.createTextNode(index);
         cell.appendChild(cellText);
         cell.addEventListener("click", function (e) {
             destroy(e.target.textContent);
         }, false);
         row.appendChild(cell);
-        index++
+        index++;
     }
 
     // add the row to the end of the table body
@@ -34,20 +34,17 @@ tbl.appendChild(tblBody);
 // appends <table> into <body>
 body.appendChild(tbl);
 // sets the border attribute of tbl to 2;
-tbl.setAttribute("border", "2");
-
+tbl.setAttribute("class", "table table-bordered w-50")
+let allCells = document.getElementsByTagName("td");
 
 
 function destroy(number) {
-    allCells = document.getElementsByTagName("td");
 
 
-    for (var i = 0; i < allCells.length; i++){
+    for (let i = 1, length = allCells.length; i <= length; i++) {
 
-        cellValue = i + 1;
-
-        if (cellValue%number === 0){
-            allCells[i].textContent = "*"
+        if (i % number === 0) {
+            allCells[i-1].textContent = "**"
         }
 
     }
